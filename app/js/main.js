577a1425cd51b9ee9c5b24f02823523507b5fb57
sava911 <pesss929@gmail.com>
2023-02-03 05:22:50 +0300

    end btn upTop

--- app/js/main.js
+++ app/js/main.js
@@ -99,11 +99,27 @@ $(function () {
   if (window.innerWidth < 500) {
     $('.single-item').slick({
       centerMode: true,
-      // centerPadding: '40px',
+      focusOnSelect: true,
       slidesToShow: 1,
-
+      useTransform: true,
       arrows: false
     });
   }
+
+  // uPTop
+  $('.back-to-top').click(function () {
+    $('body,html').animate({scrollTop: 0}, 800); // 800 - Скорость анимации
+  });
+
+  $(window).scroll(function () { // Отслеживаем начало прокрутки
+    let scrolled = $(window).scrollTop(); // Вычисляем сколько было прокручено.
+
+    if (scrolled > 350) { // Если высота прокрутки больше 350 - показываем кнопку
+      $('.back-to-top').addClass('active');
+    } else {
+      $('.back-to-top').removeClass('active');
+    }
+  });
+
 
 })
